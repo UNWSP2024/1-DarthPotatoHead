4277d6dd6825ac2cd221835b9dba440c11adf3b9
ainsley <ainsleyb2007@gmail.com>
2025-01-24 11:58:14 -0600

    Added some comments

--- program_2.py
+++ program_2.py
@@ -4,3 +4,6 @@ def proverbs_22_29():
     print('he will not stand before obscure men."\n')
     print('     (Proverbs 22:29)')
 proverbs_22_29()
+#Instead of using print messages with nothing in them for the blank lines, I included this cool \n thing.
+#I experimented earlier also and found that I could print the same message in one print statement.
+#I hope this meets the requirements!
